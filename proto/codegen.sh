3b5142d9e8847a4d647be32e9b26f7f00a6beff5
a-palchikov <deemok@gmail.com>
2015-09-16 13:47:33 -0700

    cleaned up codegen script

--- proto/codegen.sh
+++ proto/codegen.sh
@@ -3,12 +3,3 @@
 protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go servicepb/scheduler.proto servicepb/feedback.proto --go_out=plugins=grpc:.
 
 protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go storepb/store.proto --go_out=plugins=grpc,Mservicepb/scheduler.proto=github.com/a-palchikov/kron/proto/servicepb:.
-
-# for proto in *.proto
-# do
-# 	protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go $proto --go_out=plugins=grpc:.
-# done
-
-# protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go feedback.proto --go_out=plugins=grpc:.
-# protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go scheduler.proto --go_out=plugins=grpc:.
-# protoc -I. --plugin=/home/dosler/dev/go/bin/protoc-gen-go store.proto --go_out=plugins=grpc:.
